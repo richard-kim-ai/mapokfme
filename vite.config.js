a9e1624895ea472d778dcde50c60862ff040d260
Kim's winer MK16M4 <richard@kims-winer-M4.local>
2026-03-11 06:10:52 +0900

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        shinchong: resolve(__dirname, 'shinchong.html'),
+        admin: resolve(__dirname, 'admin.html')
+      }
+    }
+  },
+  server: {
+    port: 3010,
+    strictPort: true,
+    open: true
+  }
+});
